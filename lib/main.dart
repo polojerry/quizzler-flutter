af7a1f255a68d189ce74e5eed5deb055719c80fa
polojerry <polojerry4@gmail.com>
2020-05-21 11:26:00 +0300

    Added Question class for the questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,14 +26,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreList = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questions = [
+    Question(
+        question: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        question: 'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(question: 'A slug\'s blood is green.', answer: true)
   ];
 
-  List<bool> answers = [false, true, true];
+  List<Icon> scoreList = [];
 
   int questionNumber = 0;
 
@@ -43,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   void checkAnswer(bool answer) {
-    if (answers[questionNumber] == answer) {
+    if (questions[questionNumber].answerBoolean == answer) {
       print('Coreeeeeccct');
     } else {
       print("Wrongggggg");
@@ -62,7 +66,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].questionString,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionString;
+  bool answerBoolean;
+
+  Question({String question, bool answer}) {
+    questionString = question;
+    answerBoolean = answer;
+  }
+}
